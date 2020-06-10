f2679eed77cba23c60c533c9df1d5f4a7bca4427
Pradeep Pant <ppant85@gmail.com>
2020-06-10 07:14:27 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,34 @@
+class FourWheelers
+{
+   public void display()
+   {
+    System.out.println("FourWheelers displayed");
+   }
+   public void get()
+  {
+  System.out.println("Get FourWheelers");
+  display();
+  }
+}
+class Car extends FourWheelers
+{
+  public void display()
+  {
+  System.out.println("Cars displayed");
+  //super.display();
+  }
+ /* public void get(){
+    System.out.println("Get Car");
+
+  }*/
+}
+class Main{
+  public static void main(String [] args)
+ {
+  FourWheelers f=new Car();
+ // FourWheelers f1=new FourWheelers();
+  f.get();
+ // f1.get();
+ }
+ 
+}
